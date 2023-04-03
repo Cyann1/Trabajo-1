2c63709281e5b892d8915bccb5d5fc992845ec88
David <davidm.liendo@gmail.com>
2023-04-03 20:06:01 -0300

    Se agregaron metodos getters y setters.

--- TpFinal/src/Equipo.java
+++ TpFinal/src/Equipo.java
@@ -1,8 +1,20 @@
 
 public class Equipo {
 	
-	String nombre;
+	private String nombre;
+	private String descripcion;
 	
-	String descripcion;
+	public String getNombre() {
+		return nombre;
+	}
+	void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
 
 }
