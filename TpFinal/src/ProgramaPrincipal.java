c08beb285398871420e29d3200d7294890470e37
David <davidm.liendo@gmail.com>
2023-04-03 20:08:56 -0300

    Se modifico el archivo main.

--- TpFinal/src/ProgramaPrincipal.java
+++ TpFinal/src/ProgramaPrincipal.java
@@ -8,54 +8,67 @@ import javax.swing.JOptionPane;
 public class ProgramaPrincipal {
 
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-/*
+
 		Partido unPartido = new Partido();
-
-		Equipo miEquipo = new Equipo();
-		miEquipo.nombre = "Argentina";
-
-		Equipo miEquipo2 = new Equipo();
-		miEquipo2.nombre = "Arabia";
-
-		unPartido.equipo1 = miEquipo;
-		unPartido.equipo2 = miEquipo2;
-*/
-		ArrayList<Partido> listaPartidos = new ArrayList<Partido>();
-		
-		Partido unPartido;
-		
 		Equipo unEquipo1;
 		Equipo unEquipo2;
-			
-		String archivo = "archivos\\resultados";
-		for (String linea : Files.readAllLines(Paths.get(archivo))){
-		String lineas[]=linea.split(" ");
-		
-		unPartido = new Partido();
-		unEquipo1 = new Equipo();
-		unEquipo2 = new Equipo();
-		
-		unEquipo1.nombre=lineas[0];
-		unEquipo2.nombre=lineas[3];
-		
-		unPartido.equipo1=unEquipo1;
-		unPartido.equipo2=unEquipo2;
-		
-		unPartido.golesEquipo1=Integer.parseInt(lineas[1]);
-		unPartido.golesEquipo2=Integer.parseInt(lineas[2]);
-		
-		listaPartidos.add(unPartido);
-					
-		}
-		
-		for(int i=9; i<listaPartidos.size();i++) {
-			
-			JOptionPane.showMessageDialog(null, listaPartidos.get(i).equipo1.nombre+" "+listaPartidos.get(i).golesEquipo1
-					+" "+listaPartidos.get(i).golesEquipo2+" "+listaPartidos.get(i).equipo2.nombre);
-			
+
+		ArrayList<Partido> listaPartidos = new ArrayList<Partido>();
+
+		String archivo = "Archivos\\resultados.txt";
+		for (String linea : Files.readAllLines(Paths.get(archivo))) {
+			String lineas[] = linea.split(" ");
+
+			unPartido = new Partido();
+			unEquipo1 = new Equipo();
+			unEquipo2 = new Equipo();
+
+			unEquipo1.setNombre(lineas[0]);
+			unEquipo2.setNombre(lineas[3]);
+
+			unPartido.setEquipo1(unEquipo1);
+			unPartido.setEquipo2(unEquipo2);
+			unPartido.setGolesEquipo1(Integer.parseInt(lineas[1]));
+			unPartido.setGolesEquipo2(Integer.parseInt(lineas[2]));
+			listaPartidos.add(unPartido);
+
 		}
 
+		/*for (int i = 0; i < listaPartidos.size(); i++) {
+			JOptionPane.showMessageDialog(null,
+					listaPartidos.get(i).getEquipo1().getNombre() + "   " + listaPartidos.get(i).getGolesEquipo1() + "  "
+							+ listaPartidos.get(i).getGolesEquipo2() + "  " + listaPartidos.get(i).getEquipo2().getNombre());
+		}*/
+		
+		Pronostico unPronostico;
+		Equipo equipo1;
+		Equipo equipo2;
+		
+		ArrayList<Pronostico> listaPronosticos = new ArrayList<Pronostico>();
+		
+		String archivos = "Archivos\\pronostico.txt";
+		for (String linea : Files.readAllLines(Paths.get(archivos))) {
+			String lineas[] = linea.split(" ");
+
+			unPronostico = new Pronostico();
+			equipo1 = new Equipo();
+			equipo2 = new Equipo();
+			
+			equipo1.setNombre(lineas[0]);
+			equipo2.setNombre(lineas[4]);
+			
+			unPronostico.setEquipo1(equipo1);
+			unPronostico.setEquipo2(equipo2);
+			unPronostico.setGanaEquipo1(Integer.parseInt(lineas[1]));
+			unPronostico.setEmpate(Integer.parseInt(lineas[2]));
+			unPronostico.setGanaEquipo2(Integer.parseInt(lineas[3]));
+			listaPronosticos.add(unPronostico);
+		}
+		
+		/*for (int i = 0; i < listaPronosticos.size(); i++) {
+			JOptionPane.showMessageDialog(null, listaPronosticos.get(i).getEquipo1().getNombre() + "   " + listaPronosticos.get(i).getGanaEquipo1() + "  " + listaPronosticos.get(i).getEmpate() + "  "
+					+ listaPronosticos.get(i).getGanaEquipo2() + "  " + listaPronosticos.get(i).getEquipo2().getNombre());
+		}*/
 	}
 
 }
