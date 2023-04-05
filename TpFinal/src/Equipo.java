84a503fe9c25c9a58817067edff220a9fc5cb563
Cristian Carballo <cristian.carballo1994@gmail.com>
2023-04-04 21:59:47 -0300

    agregue constructores y toString() a las clases..

--- TpFinal/src/Equipo.java
+++ TpFinal/src/Equipo.java
@@ -1,18 +1,34 @@
 
 public class Equipo {
-	
+
 	private String nombre;
 	private String descripcion;
+
+	@Override
+	public String toString() {
+		return nombre;
+	}
+
+	public Equipo(String nombre, String descripcion) {
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+	}
 	
+	public Equipo() {
+	}
+
 	public String getNombre() {
 		return nombre;
 	}
-	void setNombre(String nombre) {
+
+	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public String getDescripcion() {
 		return descripcion;
 	}
+
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
--- TpFinal/src/ProgramaPrincipal.java
+++ TpFinal/src/ProgramaPrincipal.java
@@ -16,6 +16,7 @@ public class ProgramaPrincipal {
 		ArrayList<Partido> listaPartidos = new ArrayList<Partido>();
 
 		String archivo = "archivos\\resultados.txt";
+		
 		for (String linea : Files.readAllLines(Paths.get(archivo))) {
 			String lineas[] = linea.split(" ");
 
@@ -28,46 +29,54 @@ public class ProgramaPrincipal {
 
 			unPartido.setEquipo1(unEquipo1);
 			unPartido.setEquipo2(unEquipo2);
+			
 			unPartido.setGolesEquipo1(Integer.parseInt(lineas[1]));
 			unPartido.setGolesEquipo2(Integer.parseInt(lineas[2]));
+			
 			listaPartidos.add(unPartido);
-
 		}
 
 		for (int i = 0; i < listaPartidos.size(); i++) {
 			JOptionPane.showMessageDialog(null,
-					listaPartidos.get(i).getEquipo1().getNombre() + "   " + listaPartidos.get(i).getGolesEquipo1() + "  "
-							+ listaPartidos.get(i).getGolesEquipo2() + "  " + listaPartidos.get(i).getEquipo2().getNombre());
+					listaPartidos.get(i).getEquipo1().getNombre() + "   " + listaPartidos.get(i).getGolesEquipo1()
+							+ "  " + listaPartidos.get(i).getGolesEquipo2() + "  "
+							+ listaPartidos.get(i).getEquipo2().getNombre());
 		}
-		
+
 		Pronostico unPronostico;
 		Equipo equipo1;
 		Equipo equipo2;
-		
+
 		ArrayList<Pronostico> listaPronosticos = new ArrayList<Pronostico>();
-		
+
 		String archivos = "archivos\\pronostico.txt";
+		
 		for (String linea : Files.readAllLines(Paths.get(archivos))) {
 			String lineas[] = linea.split(" ");
 
 			unPronostico = new Pronostico();
 			equipo1 = new Equipo();
 			equipo2 = new Equipo();
-			
+
 			equipo1.setNombre(lineas[0]);
 			equipo2.setNombre(lineas[4]);
-			
+
 			unPronostico.setEquipo1(equipo1);
 			unPronostico.setEquipo2(equipo2);
+			
 			unPronostico.setGanaEquipo1(Integer.parseInt(lineas[1]));
 			unPronostico.setEmpate(Integer.parseInt(lineas[2]));
 			unPronostico.setGanaEquipo2(Integer.parseInt(lineas[3]));
+			
 			listaPronosticos.add(unPronostico);
 		}
-		
+
 		for (int i = 0; i < listaPronosticos.size(); i++) {
-			JOptionPane.showMessageDialog(null, listaPronosticos.get(i).getEquipo1().getNombre() + "   " + listaPronosticos.get(i).getGanaEquipo1() + "  " + listaPronosticos.get(i).getEmpate() + "  "
-					+ listaPronosticos.get(i).getGanaEquipo2() + "  " + listaPronosticos.get(i).getEquipo2().getNombre());
+			JOptionPane.showMessageDialog(null,
+					listaPronosticos.get(i).getEquipo1().getNombre() + "   " + listaPronosticos.get(i).getGanaEquipo1()
+							+ "  " + listaPronosticos.get(i).getEmpate() + "  "
+							+ listaPronosticos.get(i).getGanaEquipo2() + "  "
+							+ listaPronosticos.get(i).getEquipo2().getNombre());
 		}
 	}
 
